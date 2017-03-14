5f650aeefb3d779a60f2b1da9f894af19440982b
Lung Lao <eddielao@gmail.com>
2017-03-14 16:53:57 -0700

    Add session api util. Signup, login, and logout all working

--- frontend/elogix.jsx
+++ frontend/elogix.jsx
@@ -3,7 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as SessionAPIUtil from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>eLogix Student Tracker</h1>, root);
 });
+
+window.signup = SessionAPIUtil.signup;
+window.login = SessionAPIUtil.login;
+window.logout = SessionAPIUtil.logout;
