457e1fef9651e36b742054bc6ce00787426586ad
Lung Lao <eddielao@gmail.com>
2017-03-14 18:24:00 -0700

    Able to login using login action in console and logout with logout button

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
+import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
   <div>
     <h1>eLogix ST</h1>
+    <GreetingContainer />
     {children}
   </div>
 );
--- frontend/elogix.jsx
+++ frontend/elogix.jsx
@@ -5,9 +5,13 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
+import { login } from './actions/session_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
 });
+
+window.login=login;
