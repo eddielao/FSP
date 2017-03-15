06837ef167d4d78ebd2aae26585be74c1959da0b
Lung Lao <eddielao@gmail.com>
2017-03-15 12:40:04 -0700

    Guest login now works

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const sessionLinks = () => (
-  <nav className="login-signup">
-    <Link to="/login" activeClassName="current">Login</Link>
-    &nbsp;or&nbsp;
-    <Link to="/signup" activeClassName="current">Sign up!</Link>
-  </nav>
-);
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const personalGreeting = (currentUser, logout) => (
-	<hgroup className="header-group">
-    <h2 className="header-name">Hi, {currentUser.username}!</h2>
-    <button className="header-button" onClick={logout}>Log Out</button>
-	</hgroup>
-);
+  sessionLinks() {
+    return (
+      <nav className="login-signup">
+        <Link to="/login" activeClassName="current">Login</Link>
+        &nbsp;or&nbsp;
+        <Link to="/signup" activeClassName="current">Sign up!</Link>
+        &nbsp;or&nbsp;
+        <button onClick={() => this.props.guestLogin()}>Guest</button>
+      </nav>
+    );
+  }
 
-const Greeting = ({ currentUser, logout }) => (
-  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
-);
+  personalGreeting(currentUser, logout) {
+    return(
+      <hgroup className="header-group">
+        <h2 className="header-name">Hi, {currentUser.username}!</h2>
+        <button className="header-button" onClick={logout}>Log Out</button>
+      </hgroup>
+    );
+  }
+
+  render() {
+    let currentUser = this.props.currentUser;
+    return (
+      currentUser ?
+      this.personalGreeting(
+        currentUser, this.props.logout) : this.sessionLinks());
+  }
+}
 
 export default Greeting;
--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -1,14 +1,18 @@
 import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions';
+import { login, logout } from '../../actions/session_actions';
 import Greeting from './greeting';
 
 const mapStateToProps = ({ session }) => ({
   currentUser: session.currentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout())
-});
+const mapDispatchToProps = dispatch => {
+  const guest = { user: { username: 'guest', password: 'password'} };
+  return {
+    logout: () => dispatch(logout()),
+    guestLogin: () => dispatch(login(guest))
+  };
+};
 
 export default connect(
   mapStateToProps,
