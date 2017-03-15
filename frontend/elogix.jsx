ccfbe15072d51f6d27aaa366f32a187de486b431
Lung Lao <eddielao@gmail.com>
2017-03-14 19:16:47 -0700

    Able to login and stay logged in after page refresh

--- frontend/elogix.jsx
+++ frontend/elogix.jsx
@@ -8,7 +8,13 @@ import configureStore from './store/store';
 import { login } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
   window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
