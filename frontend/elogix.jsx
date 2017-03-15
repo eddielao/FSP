e319cb947bca640f59337f6cf453884156aa50d9
Lung Lao <eddielao@gmail.com>
2017-03-14 17:16:09 -0700

    Store getState returns default null user under session

--- frontend/elogix.jsx
+++ frontend/elogix.jsx
@@ -2,14 +2,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import * as SessionAPIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    ReactDOM.render(<h1>eLogix Student Tracker</h1>, root);
+  const store = configureStore();
+  window.store = store;
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>eLogix Student Tracker</h1>, root);
 });
-
-window.signup = SessionAPIUtil.signup;
-window.login = SessionAPIUtil.login;
-window.logout = SessionAPIUtil.logout;
