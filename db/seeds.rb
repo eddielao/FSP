5b85b7188b48aaca80cb23101186b0ec9a0d1a4b
Lung Lao <eddielao@gmail.com>
2017-03-23 11:12:31 -0700

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,10 @@ guest = User.create!(username:'Guest', password:'password')
 eddie = User.create!(username:'Eddie', password:'password')
 
 Student.destroy_all
-student1 = Student.create!(fname: 'Mia', lname: 'Lac', email: 'mia@gmail.com')
-student2 = Student.create!(fname: 'Collins', lname: 'Lac', email: 'collins@gmail.com')
-student3 = Student.create!(fname: 'Ian', lname: 'Lac', email: 'ian@gmail.com')
+mia = Student.create!(fname: 'Mia', lname: 'Lac', email: 'mia@gmail.com')
+collins = Student.create!(fname: 'Collins', lname: 'Lac', email: 'collins@gmail.com')
+ian = Student.create!(fname: 'Ian', lname: 'Lac', email: 'ian@gmail.com')
+eddie = Student.create!(fname: 'Eddie', lname: 'Lao', email: 'el@gmail.com')
 
 Course.destroy_all
 english = Course.create!(title: 'English')
@@ -21,15 +22,15 @@ math = Course.create!(title: 'Math')
 science = Course.create!(title: 'Science')
 
 Attendance.destroy_all
-attendance1 = Attendance.create!(date: '2017-01-11', course_id: math.id, student_id: student1.id)
-attendance1 = Attendance.create!(date: '2017-02-11', course_id: english.id, student_id: student2.id)
-attendance1 = Attendance.create!(date: '2017-03-11', course_id: science.id, student_id: student1.id)
-attendance1 = Attendance.create!(date: '2016-12-16', course_id: english.id, student_id: student2.id)
-attendance1 = Attendance.create!(date: '2016-12-17', course_id: math.id, student_id: student3.id)
+attendance1 = Attendance.create!(date: '2017-03-06', course_id: math.id, student_id: mia.id)
+attendance2 = Attendance.create!(date: '2017-03-07', course_id: english.id, student_id: collins.id)
+attendance3 = Attendance.create!(date: '2017-03-08', course_id: science.id, student_id: ian.id)
+attendance4 = Attendance.create!(date: '2016-03-09', course_id: english.id, student_id: eddie.id)
+attendance5 = Attendance.create!(date: '2016-03-10', course_id: math.id, student_id: mia.id)
 
 Grade.destroy_all
-grade1 = Grade.create!(grade: 'A', course_id: math.id, student_id: student1.id)
-grade2 = Grade.create!(grade: 'B', course_id: english.id, student_id: student2.id)
-grade3 = Grade.create!(grade: 'C', course_id: science.id, student_id: student1.id)
-grade4 = Grade.create!(grade: 'A', course_id: english.id, student_id: student2.id)
-grade5 = Grade.create!(grade: 'B', course_id: math.id, student_id: student3.id)
+grade1 = Grade.create!(grade: 'A', course_id: math.id, student_id: mia.id)
+grade2 = Grade.create!(grade: 'B', course_id: english.id, student_id: collins.id)
+grade3 = Grade.create!(grade: 'C', course_id: science.id, student_id: eddie.id)
+grade4 = Grade.create!(grade: 'A', course_id: english.id, student_id: mia.id)
+grade5 = Grade.create!(grade: 'B', course_id: math.id, student_id: ian.id)
