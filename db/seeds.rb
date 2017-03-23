09d330f0c7f5bdde3d9cb467dcfd99466d1acadb
Lung Lao <eddielao@gmail.com>
2017-03-23 01:51:29 -0700

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,24 @@ eddie = User.create!(username:'Eddie', password:'password')
 
 Student.destroy_all
 student1 = Student.create!(fname: 'Mia', lname: 'Lac', email: 'mia@gmail.com')
-student1 = Student.create!(fname: 'Collins', lname: 'Lac', email: 'collins@gmail.com')
-student1 = Student.create!(fname: 'Ian', lname: 'Lac', email: 'ian@gmail.com')
+student2 = Student.create!(fname: 'Collins', lname: 'Lac', email: 'collins@gmail.com')
+student3 = Student.create!(fname: 'Ian', lname: 'Lac', email: 'ian@gmail.com')
 
 Course.destroy_all
 english = Course.create!(title: 'English')
 math = Course.create!(title: 'Math')
+science = Course.create!(title: 'Science')
+
+Attendance.destroy_all
+attendance1 = Attendance.create!(date: '2017-01-11', course_id: math.id, student_id: student1.id)
+attendance1 = Attendance.create!(date: '2017-02-11', course_id: english.id, student_id: student2.id)
+attendance1 = Attendance.create!(date: '2017-03-11', course_id: science.id, student_id: student1.id)
+attendance1 = Attendance.create!(date: '2016-12-16', course_id: english.id, student_id: student2.id)
+attendance1 = Attendance.create!(date: '2016-12-17', course_id: math.id, student_id: student3.id)
+
+Grade.destroy_all
+grade1 = Grade.create!(grade: 'A', course_id: math.id, student_id: student1.id)
+grade2 = Grade.create!(grade: 'B', course_id: english.id, student_id: student2.id)
+grade3 = Grade.create!(grade: 'C', course_id: science.id, student_id: student1.id)
+grade4 = Grade.create!(grade: 'A', course_id: english.id, student_id: student2.id)
+grade5 = Grade.create!(grade: 'B', course_id: math.id, student_id: student3.id)
