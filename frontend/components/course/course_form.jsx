dd15bf1b2800d01fa684c576171249a13f77b4a5
Lung Lao <eddielao@gmail.com>
2017-03-24 09:05:51 -0700

    Remove last console log

--- frontend/components/course/course_form.jsx
+++ frontend/components/course/course_form.jsx
@@ -21,7 +21,6 @@ class CourseForm extends React.Component {
   }
 
   renderErrors() {
-    console.log(this.props.errors);
     return (
       <ul className="errors">
         {this.props.errors.map((error, i) => (
