7e1e7bd093f9ae87606b0dfaeebdfad9e8c63787
Lung Lao <eddielao@gmail.com>
2017-05-02 09:04:59 -0700

    Sidebar select unselect works

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -4,21 +4,54 @@ import CourseFormContainer from './course/course_form_container';
 import MainIndex from './main_index';
 
 const Main = ({children}) => {
+  const selectClass = e => {
+    let allA = document.getElementsByClassName("selected");
+    let eTarget = e.target;
+    // debugger;
+    Array.from(allA).forEach(aLink => {
+    // debugger;
+      if (aLink !== eTarget) {
+        aLink.className = "unselected";
+      }
+    })
+    e.target.className = "selected";
+  }
+
   return (
     <div className="main-body">
       <div className="main-side-bar">
         <ul>
-          <li><Link to="/home/index">Home</Link></li>
+          <li>
+            <Link to="/home/index" className="unselected" onClick={e => selectClass(e)}>
+              Home
+            </Link>
+          </li>
         </ul>
           <h4>ATTENDANCE</h4>
         <ul>
-          <li><Link to="attendances/add">Take Attendance</Link></li>
+          <li>
+            <Link to="attendances/add" className="unselected" onClick={e => selectClass(e)}>
+              Take Attendance
+            </Link>
+          </li>
         </ul>
           <h4>MANAGEMENT</h4>
         <ul>
-          <li><Link to="students/add">Students</Link></li>
-          <li><Link to="/courses/add">Courses</Link></li>
-          <li><Link to="/grades/add">Grades</Link></li>
+          <li>
+            <Link to="students/add" className="unselected" onClick={e => selectClass(e)}>
+              Students
+            </Link>
+          </li>
+          <li>
+            <Link to="/courses/add" className="unselected" onClick={e => selectClass(e)}>
+              Courses
+            </Link>
+          </li>
+          <li>
+            <Link to="/grades/add" className="unselected" onClick={e => selectClass(e)}>
+              Grades
+            </Link>
+          </li>
         </ul>
       </div>
       {children}
