4785109fb1b0d676457f89765a3743829a12d240
Lung Lao <eddielao@gmail.com>
2017-05-11 10:14:58 -0700

    Jest testing

---
+++ frontend/__tests__/reducers-test.js
@@ -0,0 +1,7 @@
+import StudentReducer from '../reducers/student_reducer';
+
+describe('Reducers', () => {
+  describe('StudentReducer', () => {
+    expect(typeof StudentReducer).toEqual('function');
+  });
+});
