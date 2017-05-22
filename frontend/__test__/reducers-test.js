319e7b914cbdebc7e73386f814374e229d5a0873
Lung Lao <eddielao@gmail.com>
2017-05-22 15:51:06 -0700

    Add student reducer test

---
+++ frontend/__test__/reducers-test.js
@@ -0,0 +1,9 @@
+import StudentReducer from '../reducers/student_reducer';
+
+describe('Reducers', () => {
+  describe('StudentReducer', () => {
+    it('exports an function', () => {
+      expect(typeof StudentReducer).toEqual('function');
+    });
+  });
+});  
