ead26c996be5a5cd0a18baba319655d28d6cebee
Lung Lao <eddielao@gmail.com>
2017-05-23 15:12:15 -0700

    Add 2 more tests to student reducer

--- frontend/__tests__/reducers-test.js
+++ frontend/__tests__/reducers-test.js
@@ -5,5 +5,15 @@ describe('Reducers', () => {
     it('exports an function', () => {
       expect(typeof StudentReducer).toBe('function');
     });
+
+    it('should initialize with an empty object as the default state', () => {
+      expect(StudentReducer(undefined, {})).toEqual({});
+    });
+
+    it('should return the previous state if an action is not matched', () => {
+      const oldState = { 1: 'oldState'};
+      const newState = StudentReducer(oldState, { type: 'unmatchedtype' });
+      expect(newState).toEqual(oldState);
+    });
   });
 });
