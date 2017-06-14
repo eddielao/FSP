1080c77478ff0e2c55e883cc63028e37d88f6469
Lung Lao <eddielao@gmail.com>
2017-06-14 15:56:31 -0700

    Fade in effect added with CSS

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Splash = () => {
+  /*
+  ** To make rotate background images
+  ** 1) Change this to a class component
+  ** 2) 
+  */
   return (
     <div className="splash">
       Welcome!
